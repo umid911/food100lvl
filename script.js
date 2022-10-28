7fbe556eefded7b34fcebf94f8243ecfa2a941ce
umid <muzaffarov911gmail.com>
2022-10-28 12:49:33 +0500

    startt

--- script.js
+++ script.js
@@ -31,26 +31,71 @@ const product = {
     }
 }
 
-const productBtns = document.querySelectorAll('.receipt'),
+const /* productBtns = document.querySelectorAll('.receipt'), */
     basketBtn = document.querySelector('.addCart'),
     basketModal = document.querySelector('.receipt'),
     basketClaseModal = document.querySelector('.receipt__window-btn')
 
 
-basketClaseModal.addEventListener('click', () => {
-    basketModal.classList.remove('active')
-})
-
 basketBtn.addEventListener('click', () => {
     basketModal.classList.add('active')
+    basketModal.style = `
+    display: flex;
+    opacity: 1;
+    `
 })
 
-const lvl = document.querySelector('.header__timer-extra')
+basketClaseModal.addEventListener('click', () => {
+    basketModal.classList.remove('active')
+    basketModal.style = `
+    display: none;
+    opacity: 0;
+    `
+})
 
-function logo() {
-    lvl.innerHTML++;
-    if (lvl.innerHTML < 100){
-        lvl.innerHTML = 0;
+const headerTimer = document.querySelector('.header__timer-extra')
+let i = 0    
+
+
+function timerLogo(speed = 0) {        
+    if(headerTimer.innerHTML <= 99) {        
+        i++ 
+        headerTimer.innerHTML = i
+        setTimeout(() => {
+            timerLogo()
+        }, 1000);   
+    }else {
+        i == 100
+
         
+        headerTimer.innerHTML = i
     }
+    setTimeout(() => {
+        timerLogo()
+    }, 100);
+    
+
 }
+
+timerLogo()
+
+
+function totalSum(){
+    let total = 0 
+    for(const key in product){
+        total += product[key].totalSum
+    }
+    return total
+}
+
+window.addEventListener('click', (e) => {
+    if(e.target.classList.contains('class="main__product-btn"')){
+        const atte = e.target.getAtteibute('data-symbol') 
+        const parent = e.target.closest('main__product-num')
+        if(parent){
+            const idProduct = parent.getAtteibute('id').split('_')[0]
+            if(attr == '-') product[idProduct].amount--
+            else if (atte == '+')product[idProduct].amount++
+        }
+    }
+}) 
